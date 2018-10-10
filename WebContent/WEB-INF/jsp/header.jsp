dada0a5280bbf2c00c7affe13ff439970d92cf0b
Bastien PoP <Bastien-poupelin@hotmail.fr>
2018-10-10 11:51:03 +0200

    allo

--- WebContent/WEB-INF/jsp/header.jsp
+++ WebContent/WEB-INF/jsp/header.jsp
@@ -4,7 +4,7 @@
 	
 	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
 	  <div class="container">
-		  <a class="navbar-brand" href='${param.contextPath}/welcome' >Courses</a>
+		  <a class="navbar-brand" href='${param.contextPath}/welcome'>Plateforme ECF</a>
 		  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 		    <span class="navbar-toggler-icon"></span>
 		  </button>
--- src/main/java/fr/eni/tp/qcm/bll/factory/ManagerFactory.java
+++ src/main/java/fr/eni/tp/qcm/bll/factory/ManagerFactory.java
@@ -2,8 +2,10 @@ package fr.eni.tp.qcm.bll.factory;
 
 import fr.eni.tp.qcm.bll.manager.PropositionManager;
 import fr.eni.tp.qcm.bll.manager.QuestionManager;
+import fr.eni.tp.qcm.bll.manager.UtilisateurManager;
 import fr.eni.tp.qcm.bll.manager.impl.PropositionManagerImpl;
 import fr.eni.tp.qcm.bll.manager.impl.QuestionManagerImpl;
+import fr.eni.tp.qcm.bll.manager.impl.UtilisateurManagerImpl;
 
 public class ManagerFactory {
 	
@@ -13,6 +15,10 @@ public class ManagerFactory {
 	
 	public static PropositionManager propositionManager() {
         return PropositionManagerImpl.getInstance();
+    }
+	
+	public static UtilisateurManager utilisateurManager() {
+        return UtilisateurManagerImpl.getInstance();
     }
 	
 }
