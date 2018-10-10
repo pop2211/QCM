c007a20fdb51ca509c0311aafc82bf5821e581da
Charles <charles.poullain85@gmail.com>
2018-10-10 11:44:06 +0200

    Add sectionTest DAO

--- src/main/java/fr/eni/tp/qcm/bll/factory/ManagerFactory.java
+++ src/main/java/fr/eni/tp/qcm/bll/factory/ManagerFactory.java
@@ -2,8 +2,12 @@ package fr.eni.tp.qcm.bll.factory;
 
 import fr.eni.tp.qcm.bll.manager.PropositionManager;
 import fr.eni.tp.qcm.bll.manager.QuestionManager;
+import fr.eni.tp.qcm.bll.manager.TestManager;
+import fr.eni.tp.qcm.bll.manager.ThemeManager;
 import fr.eni.tp.qcm.bll.manager.impl.PropositionManagerImpl;
 import fr.eni.tp.qcm.bll.manager.impl.QuestionManagerImpl;
+import fr.eni.tp.qcm.bll.manager.impl.TestManagerImpl;
+import fr.eni.tp.qcm.bll.manager.impl.ThemeManagerImpl;
 
 public class ManagerFactory {
 	
@@ -14,5 +18,12 @@ public class ManagerFactory {
 	public static PropositionManager propositionManager() {
         return PropositionManagerImpl.getInstance();
     }
+
+	public static ThemeManager themeManager() {
+        return ThemeManagerImpl.getInstance();
+	}
 	
+	public static TestManager testManager() {
+        return TestManagerImpl.getInstance();
+	}
 }
